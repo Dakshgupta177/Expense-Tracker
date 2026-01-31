f7f759d36bee4fd55f9365c6572489c65bf6e40d
MdLaraib25 <mdlaraib85@gmail.com>
2026-01-31 16:02:52 +0530

    made a different components

--- src/Components/Sidebar.jsx
+++ src/Components/Sidebar.jsx
@@ -1,3 +1,4 @@
+import SidebarButton from "./SidebarButton";
 
 function Sidebar() {
   return (
@@ -22,33 +23,15 @@ function Sidebar() {
         {/* Main portion */}
 
         <div className="space-y-4">
-           <div className="flex items-center content-center gap-3 hover:shadow-xl cursor-pointer rounded-4xl hover:bg-gray-50 hover:scale-102 duration-75 transition-transform h-8 ml-1  mr-5 ">
-            <span className="flex items-center content-center ml-3"><img className="w-5 h-5 " src="/icons/dashboard.png" /></span>
-            <span className="font-medium">Dashboard</span>
-          </div>
 
-           <div className="flex items-center content-center gap-3 hover:shadow-xl cursor-pointer rounded-4xl hover:bg-gray-50 hover:scale-102 duration-75 transition-transform h-8   mr-5 ">
-            <span className="flex items-center content-center ml-3"><img className="w-6 h-6" src="/icons/transaction.png" /></span>
-            <span className="font-medium">Transactions</span>
-          </div>
+           <SidebarButton icon={"/icons/dashboard.png"} text={"Dashboard"} />
+           <SidebarButton icon={"/icons/transaction.png"} text={"Transactions"} />
+           <SidebarButton icon={"/icons/budget.png"} text={"Budget"} />
+           <SidebarButton icon={"/icons/report.png"} text={"Reports"} />
+           <SidebarButton icon={"/icons/settings.png"} text={"Settings"} />
 
-           <div className="flex items-center content-center gap-3 hover:shadow-amber-50 cursor-pointer rounded-4xl hover:bg-gray-50 hover:scale-102 duration-75 transition-transform ml-0.5 h-8 mr-5 ">
-            <span className="flex items-center content-center ml-3"><img className="w-5 h-5" src="/icons/budget.png" /></span>
-            <span className="font-medium">Budget</span>
-          </div>
-           <div className="flex items-center content-center  gap-3 hover:shadow-xl cursor-pointer rounded-4xl hover:bg-gray-50 hover:scale-102 duration-75 transition-transform -ml-1.5 h-8 mr-5 ">
-            <span className="flex items-center content-center pl-2 ml-3"><img className="w-5 h-5" src="/icons/report.png" /></span>
-            <span className="font-medium">Reports</span>
-          </div>
-
-           <div className="flex items-center content-center gap-3 hover:shadow-xl cursor-pointer rounded-4xl hover:bg-gray-50 hover:scale-102 duration-200 ease-in-out transition-transform h-8 ml-1 mr-5 ">
-            <span className="flex items-center content-center ml-3"><img className="w-5 h-5" src="/icons/settings.png" /></span>
-            <span className="font-medium text-black">Settings</span>
-          </div>
         </div>
       </div>
-
-      {/* Bottom poretion */}
 
       <div className="space-y-4">
         <div className="flex items-center content-center gap-3 hover:shadow-xl cursor-pointer rounded-4xl bg-blue-400 hover:bg-blue-500 hover:scale-103 duration-300 transition-transform h-8 -ml-1 mr-5 ">
@@ -56,15 +39,8 @@ function Sidebar() {
           <span className="mb-1 -ml-1">Add Transaction</span>
         </div>
 
-         <div className="flex items-center content-center gap-3 hover:shadow-amber-50 cursor-pointer rounded-4xl hover:bg-gray-50 hover:scale-102 duration-75 transition-transform ml-0.5 h-8 mr-5 ">
-          <span className="flex items-center content-center ml-3"><img className="w-5 h-5" src="/icons/help.png" /></span>
-          <span className="font-medium">Help Center</span>
-        </div>
-
-         <div className="flex items-center content-center gap-3 hover:shadow-amber-50 cursor-pointer rounded-4xl hover:bg-gray-50 hover:scale-102 duration-75 transition-transform ml-1 h-8 mr-5 ">
-         <span className="flex items-center content-center ml-3"><img className="w-5 h-5" src="/icons/logout.png" /></span>
-          <span className="font-medium">Logout</span>
-        </div>
+        <SidebarButton icon={"/icons/help.png"} text={"Help Center"} />
+        <SidebarButton icon={"/icons/logout.png"} text={"Logout"} />
       </div>
 
     </div>
