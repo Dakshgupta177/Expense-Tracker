322f148ebff9ff06984eab611930a4dc00bece1f
MdLaraib25 <mdlaraib85@gmail.com>
2026-02-05 11:05:34 +0530

    Added the Topbar section

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Sidebar from "./Components/SideBar/Sidebar";
+import Topbar from "./Components/TopBar/Topbar";
+
+export default function App() {
+  return (
+
+    <div className="bg-[#F6F6F8]">
+      <Sidebar />
+      <Topbar />
+    </div>
+  )
+}
+
--- Frontend/src/Components/SideBar/Sidebar.jsx
+++ Frontend/src/Components/SideBar/Sidebar.jsx
@@ -2,35 +2,29 @@ import SidebarButton from "./SidebarButton";
 
 function Sidebar() {
   return (
-    <div className="bg-white w-55 min-h-screen flex flex-col justify-between px-4 py-6">
+    <div className="bg-white w-55 min-h-screen flex flex-col justify-between px-4 py-6 shadow-2xs fixed top-0 z-50">
+      {/* // <div className="bg-white w-55 h-screen fixed left-0 top-0 z-50 flex flex-col justify-between px-4 py-6 shadow-2xs"> */}
 
-      {/* Top portion */}
       <div>
 
-        {/* Logo */}
-        
-        <div className="flex items-center gap-3 mb-10">
+        <div className="flex items-center gap-3 mb-3 -mt-3.5">
           <div className="w-10 h-10 bg-gray-200 rounded-lg flex items-center justify-center">
             logo
           </div>
-
           <div>
             <h3 className="font-semibold text-lg">FinanceTracker</h3>
             <p className="text-sm text-gray-500">Personal Finance</p>
           </div>
         </div>
 
-        {/* Main portion */}
-
-        <div className="space-y-4">
-
+        <div className="space-y-4 mt-1">
            <SidebarButton icon={"/icons/dashboard.png"} text={"Dashboard"} />
            <SidebarButton icon={"/icons/transaction.png"} text={"Transactions"} />
            <SidebarButton icon={"/icons/budget.png"} text={"Budget"} />
            <SidebarButton icon={"/icons/report.png"} text={"Reports"} />
            <SidebarButton icon={"/icons/settings.png"} text={"Settings"} />
-
         </div>
+
       </div>
 
       <div className="space-y-4">
